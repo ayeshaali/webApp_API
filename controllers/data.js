a0abf9e27f20d95924906e11e40f35ead1b7b621
Ayesha <aliayeshanyc@gmail.com>
2019-04-22 09:26:45 -0400

    commit

--- controllers/data.js
+++ controllers/data.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var fs = require("fs");
 var router = express.Router();
+var apikey = 'f09c17eb';
+var Saved = require('../models/saved');
+
 
 router.get('/search', function(request, response) {
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
     response.render('search');
+})
+
+router.get('/mysaved', function(request, response) {
+    Saved.getSave();
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render('mysaved');
+})
+
+router.post("/jobs", function(req,res){
+  request("APIintheSky.herokuapp.com/jobsearch?apikey="+apikey+"&agency="+req.query.agency+"&title="+req.query.title+"&category="+req.query.category+"&service="+req.query.service+"&location="+req.query.location, function(err, response, body) {
+      if(!err){
+        var data = JSON.parse(body);
+        Saved.savedInfo(data,"benefits", function(response)){
+          res.render('mysaved.ejs', {movie: response})
+        };
+      }
+      else{
+        res.redirect('/search');
+      }
+
+    });//look for the movie
+})
+
+router.post("/benefits", function(request,response){
+  request("APIintheSky.herokuapp.com/benefitsearch?apikey="+apikey+"&name="+req.query.name+"&type="+req.query.type+"&pop="+req.query.pop+"&contact="+req.query.contact+"&desc="+req.query.desc, function(err, response, body) {
+      if(!err){
+        var data = JSON.parse(body);
+        Saved.savedInfo(data,"benefits", function(response)){
+          res.render('mysaved.ejs', {movie: response})
+        };
+      }
+      else{
+        res.redirect('/search');
+      }
+
+    });//look for the movie
+})
+
+router.delete('/jobs/:id', function (req, res) {
+  Jobs.deleteUser(req.params.id, function(){
+    res.status(200);
+    res.setHeader('Content-Type', 'text/html')
+    res.render('mysaved');
+  });
+})
+
+router.delete('/jobs/:id', function (req, res) {
+  Jobs.deleteUser(req.params.id, function(){
+    res.status(200);
+    res.setHeader('Content-Type', 'text/html')
+    res.render('mysaved');
+  });
 })
